33eaa0fd4f2e49986ea4c467cb831704b03d4664
Anthony <anthony.ladas@gmail.com>
2022-12-14 14:12:56 +0000

    nested bot to determine function

--- ChatBot/our_chatbot.py
+++ ChatBot/our_chatbot.py
@@ -4,11 +4,13 @@ from chatterbot.response_selection import get_most_frequent_response
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
+import tensorflow as tf
+
 # Create a new chatbot named Charlie
 chatbot = ChatBot(
-    'Terminal',
+    'chatbot',
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    database_uri='sqlite:///database.db',
+    database_uri='sqlite:///chatbot.db',
     read_only=True,
 
     preprocessors=[
@@ -17,31 +19,27 @@ chatbot = ChatBot(
 
     logic_adapters=[
         {
-            'import_path': 'chatterbot.logic.SpecificResponseAdapter',
-            'input_text': 'help',
-            'output_text': 'Confluence like to supported functions: chatterbot.rtfd.org'
-        },
-        {
             'import_path': 'price_logic_adapter.PriceLogicAdapter'
         },
         {
-            "import_path": "chatterbot.logic.BestMatch",
+            'import_path': 'chatterbot.logic.BestMatch',
             "statement_comparison_function": LevenshteinDistance,
-            "response_selection_method": get_most_frequent_response
+            'default_response': 'I am sorry, but I do not understand.',
+            'maximum_similarity_threshold': 0.85
         },
-        'chatterbot.logic.MathematicalEvaluation'
+        'chatterbot.logic.MathematicalEvaluation',
     ]
 )
 
-# corpusTrainer = ChatterBotCorpusTrainer(chatbot)
-# corpusTrainer.train(
-#     "chatterbot.corpus.english.conversations"
-# )
+corpusTrainer = ChatterBotCorpusTrainer(chatbot)
+corpusTrainer.train(
+    "chatterbot.corpus.english"
+)
 
 listTrainer = ListTrainer(chatbot)
-price_talk = [['get me the price for A', 'its 5'],
-              ['what is the price of A', 'its 5'],
-              ['current price of A', 'its 5']]
+help_response = 'Confluence link to supported functions: chatterbot.rtfd.org'
+help_talk = ['help', 'help me', 'what can i ask', 'possible functions', 'what possible functions can you run', 'what can you do']
+for i in help_talk:
+    listTrainer.train([i, help_response])
 
-for i in price_talk:
-    listTrainer.train(i)
+
--- ChatBot/price_logic_adapter.py
+++ ChatBot/price_logic_adapter.py
@@ -1,20 +1,51 @@
 from chatterbot.logic import LogicAdapter
 
+price_talk = [['get me the price for', 'PRICE'],
+              ['what is the price of', 'PRICE'],
+              ['current price of', 'PRICE'],
+              ['current price', 'PRICE'],
+              ['when was the latest vol for', 'VOL'],
+              ['latest vol surface for', 'VOL'],
+              ['latest vol timestamp', 'VOL'],
+              ['latest vol save', 'VOL']]
+
 class PriceLogicAdapter(LogicAdapter):
     def __init__(self, chatbot, **kwargs):
         super().__init__(chatbot, **kwargs)
+        from chatterbot import ChatBot
+        from chatterbot.trainers import ListTrainer
+
+        self.inner_chatbot = ChatBot(
+            'PriceLogicBot',
+            storage_adapter='chatterbot.storage.SQLStorageAdapter',
+            database_uri='sqlite:///PriceLogicBot.db',
+            read_only=True,
+            logic_adapters=[
+                {
+                    'import_path': 'chatterbot.logic.BestMatch',
+                    'maximum_similarity_threshold': 0.20,
+                    'default_response': 'NO',
+                }
+            ]
+        )
+
+        list_trainer = ListTrainer(self.inner_chatbot)
+        for i in price_talk:
+            list_trainer.train(i)
 
     def can_process(self, statement):
-        words = ['get', 'price']
-        if all(x in statement.text.split() for x in words):
-            return True
-        else:
-            return False
+        bot_input = self.inner_chatbot.get_response(statement.text)
+        return bot_input.text != "NO"
 
     def process(self, input_statement, additional_response_selection_parameters):
+        print('starting process')
+
         import random
         from chatterbot.conversation import Statement
         import requests
+
+        bot_input = self.inner_chatbot.get_response(input_statement.text)
+        print("doing " + bot_input.text)
 
         response = requests.get('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo')
         data = response.json()
@@ -22,7 +53,7 @@ class PriceLogicAdapter(LogicAdapter):
         # Randomly select a confidence between 0 and 1
         # confidence = random.uniform(0, 1)
         if response.status_code == 200:
-            confidence = 0.8
+            confidence = 1
         else:
             confidence = 0
 
@@ -30,7 +61,7 @@ class PriceLogicAdapter(LogicAdapter):
         response_statement = Statement(text='The current temperature is {}'.format(price))
         response_statement.confidence = confidence
 
-        ric=''
+        ric = ''
 
         print(input_statement, additional_response_selection_parameters)
 
